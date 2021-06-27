374f28bba4e4d170878a0829c7da22531963e174
Leonardo Adamoli <leo.adamoli@gmail.com>
2021-06-27 10:10:56 -0300

    :lipstick: Small adjustment on Button component styles

--- Project_2/src/components/UI/Button/Button.styles.js
+++ Project_2/src/components/UI/Button/Button.styles.js
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
 
 export const __button = css`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -8,6 +9,10 @@ export const __button = css`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media screen and (min-width: 767px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
